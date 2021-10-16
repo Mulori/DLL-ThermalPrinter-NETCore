9cb455a74b1ed4a37b6aa80c8f9c7255044a9142
mulori <murilogarcia.hr@gmail.com>
2021-10-16 01:01:39 -0300

    primeiro commit

---
+++ UserControl1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LibraryThermalPrinter_NET_Core
+{
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+    }
+}
